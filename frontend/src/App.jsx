e958af89d0b66bd3b5f57b0eb46e20dd14e6021e
DinoFazlic <dino.fazlic10@gmail.com>
2025-04-18 13:49:10 +0200

    Inital commit: Project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles/App.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}> {}
+      <h1>Info Student</h1>
+    </div>
+  );
+}
+
+export default App;
