3d1126cbb506450170a98995ffafffcbe18bd602
Edah Ždralović <edahzdralovic@192.168.1.17>
2025-06-05 14:09:36 +0200

    radi ovo je brench za kartice koje rade

--- backend/app/models/employer_profile.py
+++ backend/app/models/employer_profile.py
@@ -2,11 +2,5 @@ from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
 from .users import User
 
-class Employers(SQLModel, table=True):
-    user_id: int = Field(foreign_key="user.id", primary_key=True)
-    company_name: Optional[str]
-    company_description: Optional[str]
-    address: Optional[str]
-    website_url: Optional[str]
 
     user: Optional[User] = Relationship(back_populates="employer_profile")
